e5429a8ff146a833523d5cd9d5f5364e4efc7dd4
Isaque Carlos da Silva Melo <isaquecarlos2016@gmail.com>
2026-03-28 21:11:54 -0300

    inicializando o projeto

---
+++ src/main/java/senac/tsi/minha_saude/MinhaSaudeApplication.java
@@ -0,0 +1,13 @@
+package senac.tsi.minha_saude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaSaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senac/tsi/minha_saude/MinhaSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package senac.tsi.minha_saude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
